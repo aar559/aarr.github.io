2d5c4d808cbde4e674eff6efbd5000895e1881b2
Aaryan Bhati <aaryanbhati05@gmail.com>
2024-11-25 10:35:55 +1100

    1.2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Add this line since you're using a custom domain
-})
-
+  base: '/', // Correct for a custom domain like aaryanbhati.com
+});
 
