b57402ff661e6cd43981a4f607540fe1a21f6ae3
Aaryan Bhati <aaryanbhati05@gmail.com>
2024-11-25 08:58:19 +1100

    dev1.1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Add this line since you're using a custom domain
 })
+
